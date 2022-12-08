f3e34a759fd6fd295929261ba15c1370b654740a
ngabo-dev <ngabo470@gmail.com>
2022-12-08 12:59:35 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,37 @@
+/*
+ *
+ *  * File: 4-puts.c
+ *
+ *   * Auth: Brennan D Baraban <375@holbertonschool.com>
+ *
+ *    */
+
+
+
 #include <stdio.h>
+
+
+
 /**
- * main - main block
- * Return: o 
- */
+ *
+ *  * main - Prints "Programming is like building a multilingual
+ *
+ *   *                puzzle, followed by a new line.
+ *
+ *    *
+ *
+ *     * Return: Always 0.
+ *
+ *      */
+
 int main(void)
+
 {
-          puts("\"Programming is like building a multilingual puzzle");
-          return(0);
-	  betty 4-puts.c
-}	  
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
+}
