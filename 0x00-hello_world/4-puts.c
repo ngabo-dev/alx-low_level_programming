4874b14a0eb5414272c8c66679348e77b9cd4809
ngabo-dev <ngabo470@gmail.com>
2022-12-12 04:08:26 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints "programming is like building a multilingual 
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"programming is like building a multilingual puzzle");
 
-}	
+	return (0);
+}
+
+
