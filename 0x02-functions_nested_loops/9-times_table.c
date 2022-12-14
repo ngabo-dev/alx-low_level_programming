65bd0b852c155ab70ec742e43e23c906a274d36b
ngabo-dev <ngabo470@gmail.com>
2022-12-13 17:51:23 -0800

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,20 +15,20 @@ void times_table(void)
 			c = a * b;
 			if ((c / 10) == 0)
 			{
-				if(b == 0)
+				if (b == 0)
 				{
-					_putchar('0');
+				_putchar('0');
 				}
 
 				if (b != 0)
 				{
-					_putchar(' ');
-					_putchar((c % 10) + '0');
+				_putchar(' ');
+				_putchar((c % 10) + '0');
 				}
 				if (b < 9)
 				{
-					_putchar(',');
-					_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 				}
 			}
 			else
@@ -37,8 +37,8 @@ void times_table(void)
 				_putchar((c % 10) + '0');
 				if (b < 9)
 				{
-					_putchar(',');
-					_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 				}
 			}
 		}
