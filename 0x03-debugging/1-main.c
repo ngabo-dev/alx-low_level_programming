64c5646b5221a3e6479785ff5c2c03287141feca
ngabo-dev <ngabo470@gmail.com>
2022-12-15 06:27:38 -0800

    task 1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,18 +6,21 @@
  */
 
 int main(void)
+
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+	
 	/*
 	 * while (i < 10)
 	 * {
 	 * putchar(i);
 	 * }
 	 */
+	
 	printf("Infinite loop Avoided! \\o\/\n");
 
 	return (0);
