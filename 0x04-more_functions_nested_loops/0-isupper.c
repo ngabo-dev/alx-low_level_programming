3f7813a2e4b800d420a00d856a650db8b1d14e09
ngabo-dev <ngabo470@gmail.com>
2022-12-15 13:06:49 -0800

    task 0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - chekes upper case
+ * @c: print int
+ * Return: give 1 or 0
+ */
+int _uppercase(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
+
+
