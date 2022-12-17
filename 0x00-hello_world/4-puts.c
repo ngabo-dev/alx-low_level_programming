2222bbb3d2ebfde51e2550a1d091a9187631a5b6
ngabo-dev <ngabo470@gmail.com>
2022-12-17 08:50:32 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual 
  * puzzle, followed by a newline.
  * Return: Always 0.
  */
@@ -10,7 +10,7 @@
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 
 }
