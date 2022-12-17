2cafd1d1782860aec5ec8bc84838f8bfb3d3d005
ngabo-dev <ngabo470@gmail.com>
2022-12-17 09:57:18 -0800

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,14 @@
 
 
 /**
- * main - Entry point
- *
- * Return:1
+ * main - prints exactly "and that piece of art is useful"
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2. "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }	
