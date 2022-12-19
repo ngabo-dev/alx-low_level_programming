0cef01697daace792f0a005f270a8f636a10dd5d
ngabo-dev <ngabo470@gmail.com>
2022-12-19 08:05:26 -0800

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - generates random valid passwords
  * Return: 0 (on success).
  *
  */
+
 int main(void)
 {
 	int pass, sum;
