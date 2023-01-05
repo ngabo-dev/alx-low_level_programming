a457aa95a055f739bc8e5977529462bfa40f9560
ngabo-dev <ngabo470@gmail.com>
2023-01-05 13:25:54 -0800

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,3 +20,15 @@ int _evaluate(int i, int n)
 	return (-1);
 	return (-1);
 }
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+
+	else
+	{
+		return (_evaluate(i, n));
+	}
+}
