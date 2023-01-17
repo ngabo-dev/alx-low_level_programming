eef4addb583ae6e70580bd61a582e5d26a487331
ngabo-dev <ngabo470@gmail.com>
2023-01-17 02:05:29 -0800

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  a program that prints the name of the file it was compiled from, followed by a new line.
+ *
+ * Return: 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
