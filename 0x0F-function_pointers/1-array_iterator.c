146ae8bc2df795bb875a7dc337d54d42f8b199ee
ngabo-dev <ngabo470@gmail.com>
2023-01-19 06:38:38 -0800

    1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 
-int is_even(int num) {
-    return (num % 2 == 0);
+void array_iterator(int *array, size_t size, void (*action)(int)) {
+    for (size_t i = 0; i < size; i++) {
+        action(array[i]);
+    }
 }
 
-int int_index(int *array, int size, int (*cmp)(int)) {
-    if (size <= 0) {
-        return -1;
-    }
-    for (int i = 0; i < size; i++) {
-        if (cmp(array[i]) != 0) {
-            return i;
-        }
-    }
-    return -1;
+void print_int(int num) {
+    printf("%d ", num);
+}
+
+void double_int(int *num) {
+    *num = *num * 2;
 }
 
 int main() {
     int array[] = {1, 2, 3, 4, 5};
-    int size = sizeof(array) / sizeof(array[0]);
-    int index = int_index(array, size, is_even);
-    if (index == -1) {
-        printf("No even number found in the array.");
-    } else {
-        printf("The first even number in the array is at index: %d", index);
-    }
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Original array: ");
+    array_iterator(array, size, print_int);
+    printf("\n");
+
+    array_iterator(array, size, double_int);
+    printf("Doubled array: ");
+    array_iterator(array, size, print_int);
+    printf("\n");
+
     return 0;
 }
 
