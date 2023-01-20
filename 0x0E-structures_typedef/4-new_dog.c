2adaae6936e1e2bc5ce6d35a1bd6eda92c9f5c72
ngabo-dev <ngabo470@gmail.com>
2023-01-20 03:59:45 -0800

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
+ * *new_dog - creates a new dog
  * @name: 1st member.
  * @age: 2nd member.
  * @owner: 3rd member.
@@ -12,7 +12,7 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, lenN, lenO;
+	int i, lenN, len;
 
 	struct dog *n_dog = NULL;
 
@@ -23,31 +23,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 	while (owner[lenO] != '\0')
 		lenO++;
 
-	n_dog = malloc(sizeof(struct dog));
-	if (n_dog == NULL)
-	{
-		free(n_dog);
-		return (NULL);
-	}
-	n_dog->name == malloc(lenN + 1);
-	if (n_dog->name == NULL)
-	{
-		free(n_dog->name);
-		free(n_dog);
-		return (NULL);
-	}
-	n_dog->owner = malloc(lenO + 1);
-	if (n_dog->owner == NULL)
-	{
-		free(n_dog->name);
-		free(n_dog->owner);
-		free(n_dog);
-		return (NULL);
-	}
-	for (i = 0; i <= lenN; i++;)
-		n_dog->name[i] = name[i];
-	for (i = 0; i <= lenO; i++)
-		n_dog->owner[i] = owner[i];
-	n_dog->age = age;
-	return (n_dog);
-}
+	n_dog->namr = malloc(lenN )
