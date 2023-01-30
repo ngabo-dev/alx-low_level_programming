c2c9d74a5cafc9fb86396f5def6d977be885a9ff
ngabo-dev <ngabo470@gmail.com>
2023-01-30 02:37:54 -0800

    tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,11 +18,15 @@ list_t *add_node(list_t **head, const char *str)
 		len++;
 
 	new = malloc(sizeof(list_t));
+
 	if (!new)
 		return (NULL);
+
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
+
 	return (*head);
 }
+
