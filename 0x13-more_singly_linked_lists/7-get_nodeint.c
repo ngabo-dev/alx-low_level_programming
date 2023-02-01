10a28c8f63c6963245811e0191ca6b9a07d741f4
ngabo-dev <ngabo470@gmail.com>
2023-02-01 10:39:04 -0800

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	
+
 	listint_t *temp = head;
 	while (temp && i < index)
 	{
@@ -20,4 +20,3 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	return (temp ? temp : NULL);
 }
-
