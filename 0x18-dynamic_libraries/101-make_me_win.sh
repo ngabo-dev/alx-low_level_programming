91b7481d557a3a03676187ba1f8fd99b3e6aa1f0
ngabo-dev <ngabo470@gmail.com>
2023-03-22 05:07:56 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
-gcc -fpic -c *.c
-gcc -shared -o 100-operations.so *.o
+#!/bin/bash
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
