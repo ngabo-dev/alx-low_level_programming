28afde3945b3de1dc861417167a6f1f46d7c313f
ngabo-dev <ngabo470@gmail.com>
2023-11-24 02:23:34 -0800

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,20 @@
  *
  * Return: the converted number
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *b);
 {
-	int i;
-	unsigned int dec_val = 0;
-
-	if (!b)
-		return (0);
-
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+	if (b == NULL) {
+		return 0;
 	}
 
-	return (dec_val);
+	unsigned int result = 0;
+	while (*b != '\0') {
+		if (*b == '0' || *b == '1') {
+			result = (result << 1) + (*b - '0');
+			b++;
+			} else {
+				return 0;
+			}
+	}
+	return result;
 }
