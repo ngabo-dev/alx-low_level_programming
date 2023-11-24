46feab42bc6d503202a15e89d28cc767487ece4b
ngabo-dev <ngabo470@gmail.com>
2023-11-24 02:39:29 -0800

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,22 @@
-#include "main.h"
+#include <stdio.h>
+#include <limits.h>
 
-/**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
- */
-unsigned int binary_to_uint(const char *b);
-{
-	if (b == NULL) {
-		return 0;
-	}
+unsigned int binary_to_uint(const char *b) {
+    if (b == NULL) {
+        return UINT_MAX; // Return a special value for NULL input
+    }
 
-	unsigned int result = 0;
-	while (*b != '\0') {
-		if (*b == '0' || *b == '1') {
-			result = (result << 1) + (*b - '0');
-			b++;
-			} else {
-				return 0;
-			}
-	}
-	return result;
+    unsigned int result = 0;
+
+    while (*b != '\0') {
+        if (*b == '0' || *b == '1') {
+            result = (result << 1) + (*b - '0');
+            b++;
+        } else {
+            // Return a special value for invalid characters in the string
+            return UINT_MAX;
+        }
+    }
+
+    return result;
 }
